1ea0e62a45e5adaf208438e00c05c48947a21140
Adam Griffiths <aogriffiths@gmail.com>
2018-06-08 11:11:02 +0100

    updating bat scripts

--- start.bat
+++ start.bat
@@ -1,17 +1,17 @@
-SET CONTAINER_NAME=jupyter-power-container
-SET HOME_DIR=%userprofile%\%CONTAINER_NAME%-home
-if not exist "%HOME_DIR%" mkdir %HOME_DIR%
-TOKEN=ABCD
+$CONTAINER_NAME="jupyter-power-container"
+$HOME_DIR="$env:USERPROFILE\$CONTAINER_NAME-home"
+New-Item -ItemType Directory -Force -Path $HOME_DIR
+$TOKEN=(-join ((65..90) + (97..122) | Get-Random -Count 32 | % {[char]$_}))
 
 echo "docker container id:"
-docker run --rm -d ^
-  -v /var/run/docker.sock:/var/run/docker.sock ^
-  --group-add 50 ^
-  -v %HOME_DIR%:/home/poweruser ^
-  -p 8888:8888 ^
-  northhighland/jupyter-power-container:latest ^
-  jupyter notebook --ip=0.0.0.0 --NotebookApp.token=%TOKEN%
+docker run --rm -d \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --group-add 50 \
+  -v $HOME_DIR:/home/poweruser \
+  -p 8888:8888 \
+  $CONTAINER_NAME:latest \
+  jupyter notebook --ip=0.0.0.0 --NotebookApp.token=$TOKEN
 
 echo ""
 echo "URL:"
-echo "http://localhost:8888/?token="%TOKEN%
+echo "http://localhost:8888/?token=$TOKEN"
--- stop.bat
+++ stop.bat
@@ -1,5 +1,5 @@
-CONTAINER_NAME=jupyter-power-container
-CONTAINER_IDS=$(docker ps -a -q --filter ancestor=$CONTAINER_NAME --format="{{.ID}}")
+$CONTAINER_NAME="jupyter-power-container"
+$CONTAINER_IDS=$(docker ps -a -q --filter ancestor=$CONTAINER_NAME --format="{{.ID}}")
 if [  -n "$CONTAINER_IDS" ]
 then
   echo "stoping jupyter-power-container docker containers:"
