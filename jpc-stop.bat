ea12496b7770b2af9625d819fc5552d2a2c777ba
Adam Griffiths <aogriffiths@gmail.com>
2018-06-08 14:33:27 +0100

    added a scoop app

--- jpc-stop.bat
+++ jpc-stop.bat
@@ -1,6 +1,6 @@
 @echo off
 SET CONTAINER_NAME=jupyter-power-container
-$CONTAINER_IDS=$(docker ps -a -q --filter ancestor=%CONTAINER_NAME% --format="{{.ID}}")
+SET CONTAINER_IDS=$(docker ps -a -q --filter=ancestor=northhighland/jupyter-power-container:latest --format="{{.ID}}")
 if [  -n "$CONTAINER_IDS" ]
 then
   echo "stoping jupyter-power-container docker containers:"
